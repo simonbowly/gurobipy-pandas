3a0b6c0c13b99ff530b1b700b91d4df40f87a255
Simon Bowly <bowly@gurobi.com>
2023-06-18 09:33:14 +1000

    Implement __getitem__

--- src/gurobipy_pandas/extension.py
+++ src/gurobipy_pandas/extension.py
@@ -31,3 +31,18 @@ class GurobiVarArray(ExtensionArray):
     @property
     def dtype(self) -> ExtensionDtype:
         return GurobiVarDtype()
+
+    def __getitem__(self, item):
+        """
+        Pandas has clear guidance here in ExtensionDType:
+
+        - For scalar ``item``, return a scalar value suitable for the array's
+          type. This should be an instance of ``self.dtype.type``.
+        - For slice ``key``, return an instance of ``ExtensionArray``, even if
+          the slice is length 0 or 1.
+        - For a boolean mask, return an instance of ``ExtensionArray``, filtered
+          to the values where ``item`` is True.
+        """
+        if isinstance(item, int):
+            return self.mvar[item].item()
+        return GurobiVarArray(self.mvar[item])  # TODO does this need a copy?
