fa518f679d6184b009db55c5a8cfb47fb3f3cedf
Simon Bowly <bowly@gurobi.com>
2023-06-18 09:33:02 +1000

    Add tests

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,5 +1,7 @@
 import unittest
 
+import gurobipy as gp
+import numpy as np
 from pandas.api.types import pandas_dtype, is_extension_array_dtype
 
 from gurobipy_pandas.extension import GurobiVarArray, GurobiVarDtype
@@ -24,3 +26,47 @@ class TestCreateGurobiVarArray(GurobiModelTestCase):
         mvar = self.model.addMVar((10,))
         arr = GurobiVarArray(mvar)
         self.assertIsInstance(arr.dtype, GurobiVarDtype)
+
+
+class TestGurobiVarArrayGetItem(GurobiModelTestCase):
+    def setUp(self):
+        super().setUp()
+        mvar = self.model.addMVar((10,), name="x")
+        self.arr = GurobiVarArray(mvar)
+        self.model.update()
+
+    def test_scalar(self):
+        # For scalar ``item``, return a scalar value suitable for the array's
+        # type. This should be an instance of ``self.dtype.type``.
+        for i in range(10):
+            obj = self.arr[i]
+            self.assertIsInstance(obj, gp.Var)
+            self.assertEqual(obj.VarName, f"x[{i}]")
+
+    def test_slice_1(self):
+        # For slice ``key``, return an instance of ``ExtensionArray``, even
+        # if the slice is length 0 or 1.
+        for i in range(10):
+            obj = self.arr[i : i + 1]
+            self.assertIsInstance(obj, GurobiVarArray)
+            self.assertEqual(len(obj), 1)
+            self.assertEqual(obj[0].VarName, f"x[{i}]")
+
+    def test_slice_2(self):
+        # For slice ``key``, return an instance of ``ExtensionArray``, even
+        # if the slice is length 0 or 1.
+        obj = self.arr[1:6:2]
+        self.assertIsInstance(obj, GurobiVarArray)
+        self.assertEqual(len(obj), 3)
+        for i in range(3):
+            self.assertEqual(obj[i].VarName, f"x[{2*i + 1}]")
+
+    def test_mask(self):
+        # For a boolean mask, return an instance of ``ExtensionArray``, filtered
+        # to the values where ``item`` is True.
+        mask = np.array([0, 1, 0, 1, 0, 1, 0, 1, 0, 1]).astype(bool)
+        obj = self.arr[mask]
+        self.assertIsInstance(obj, GurobiVarArray)
+        self.assertEqual(len(obj), 5)
+        for i in range(5):
+            self.assertEqual(obj[i].VarName, f"x[{2*i + 1}]")
