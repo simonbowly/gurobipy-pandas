4e102047079a9a5f27bd10e0c896ba75c69db661
Simon Bowly <bowly@gurobi.com>
2023-06-18 12:12:23 +1000

    Add unordered expression equality utils

--- tests/utils.py
+++ tests/utils.py
@@ -3,6 +3,11 @@ import unittest
 import gurobipy as gp
 
 
+def sorted_linear_terms(linexpr):
+    terms = [(linexpr.getVar(i), linexpr.getCoeff(i)) for i in range(linexpr.size())]
+    return sorted(terms, key=lambda term: (id(term[0]), term[1]))
+
+
 class GurobiModelTestCase(unittest.TestCase):
     def setUp(self):
         self.env = gp.Env()
@@ -36,3 +41,21 @@ class GurobiModelTestCase(unittest.TestCase):
             self.assertTrue(expr1.getVar1(i).sameAs(expr2.getVar1(i)))
             self.assertTrue(expr1.getVar2(i).sameAs(expr2.getVar2(i)))
             self.assertEqual(expr1.getCoeff(i), expr2.getCoeff(i))
+
+    def assert_expression_equal_unordered(self, expr1, expr2):
+        if isinstance(expr1, gp.LinExpr):
+            self.assert_linexpr_equal_unordered(expr1, expr2)
+        elif isinstance(expr1, gp.QuadExpr):
+            assert False
+
+    def assert_linexpr_equal_unordered(self, expr1, expr2):
+        # Order-independent (no grouping of like terms)
+        self.assertIsInstance(expr1, gp.LinExpr)
+        self.assertIsInstance(expr2, gp.LinExpr)
+        self.assertEqual(expr1.getConstant(), expr2.getConstant())
+        self.assertEqual(expr1.size(), expr2.size())
+        for (var1, coeff1), (var2, coeff2) in zip(
+            sorted_linear_terms(expr1), sorted_linear_terms(expr2)
+        ):
+            self.assertTrue(var1.sameAs(var2))
+            self.assertEqual(coeff1, coeff2)
